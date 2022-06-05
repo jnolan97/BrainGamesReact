121d87accdf9b9fce29cf524d657e02b7feaa6a2
jnolan97 <johnmnolan97@gmail.com>
2022-06-05 11:59:42 -0400

    Fixed inline-block for panel

--- src/digitspan/components/Panel.jsx
+++ src/digitspan/components/Panel.jsx
@@ -1,34 +1,20 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import styled from 'styled-components';
-
+import '../../styles/main.css';
 export default function Panel() {
-  //   const Button = styled.Button`
-  //   && {
-  //   border-radius: "50%";
-  //   height: 90;
-  //   width: 90;
-  //   display: inline;
-  //   }
-  // `;
-
-  //   const Container = styled.div`
-  //   text-align: center;
-  // `
-
   const panel_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
     <div>
       {panel_array.map(btn => (
-        <div className='button-group'>
-          <Button variant="outlined" color="primary"
-            sx={{
-              width: 300,
-              '& .MuiButton-root': {
-                borderRadius: '50%',
-                display: "inline-block"
-              },
-            }}>
+        <div className='button-group' style={{ display: 'inline-block', flexDirection: 'row' }}>
+          <Button variant="outlined" color="primary" sx={{
+            width: 90,
+            height: 90,
+            display: "block",
+            borderRadius: "50%",
+            mr: 2
+          }}>
             {btn}
           </Button>
         </div>
