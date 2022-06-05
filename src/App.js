7f3dffe3b84d78143077b96d6702d1942d91d014
jnolan97 <johnmnolan97@gmail.com>
2022-06-04 21:49:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        BrainGames Frontend project!
         </p>
         <a
           className="App-link"
